1b5d528fd6ee1baaff74c85d31e5a01b4af0d331
Sean Cannon <seancannon@gmail.com>
2020-12-06 23:06:01 -0500

    🔨 add javascript to handle menu

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,15 @@
 // This is all you.
+const menuButton = document.querySelector('#menu-button');
+const menuList = document.querySelector('#menu-list');
+
+function handleMenu() {
+    if (menuList.classList.contains('block')) {
+        menuList.classList.remove('block');
+        menuList.classList.add('hidden');
+    } else {
+        menuList.classList.remove('hidden');
+        menuList.classList.add('block');
+    }
+}
+
+menuButton.addEventListener('click', handleMenu);
