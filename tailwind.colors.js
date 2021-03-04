a62c8d6dce4f3816ac9907402d361a2669ae039c
Sean Cannon <seancannon@gmail.com>
2021-03-03 22:49:37 -0500

    color changes

---
+++ tailwind.colors.js
@@ -0,0 +1,19 @@
+colors: {
+  background: '#232946',
+  headline: '#fffffe',
+  paragraph: '#b8c1ec',
+  'paragraph-white': '#fefefe',
+  button: '#eebbc3',
+  'button-text': '#232946',
+  stroke: '#121629',
+  main: '#b8c1ec',
+  highlight: '#eebbc3',
+  secondary: '#fffffe',
+  tertiary: '#eebbc3',
+  'light-background': '#d4d8f0',
+  'light-accent': '#f0e2e1',
+
+  'rich-black': '#0C1824',
+  'blue-green': '#77CBB9',
+  orange: '#FFA552'
+},
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,36 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: {
         content: ['./resources/**/*.antlers.html', './resources/**/*.blade.php', './content/**/*.md'],
     },
     important: true,
     theme: {
-        fontFamily: {
+        extend: {
+          fontFamily: {
             sans: [
-                'Inter',
-                '-apple-system',
-                'BlinkMacSystemFont',
-                '"Segoe UI"',
-                'Roboto',
-                '"Helvetica Neue"',
-                'Arial',
-                '"Noto Sans"',
-                'sans-serif',
-                '"Apple Color Emoji"',
-                '"Segoe UI Emoji"',
-                '"Segoe UI Symbol"',
-                '"Noto Color Emoji"',
+              '-apple-system',
+              'BlinkMacSystemFont',
+              '"Segoe UI"',
+              'Roboto',
+              '"Helvetica Neue"',
+              'Arial',
+              '"Noto Sans"',
+              'sans-serif',
+              '"Apple Color Emoji"',
+              '"Segoe UI Emoji"',
+              '"Segoe UI Symbol"',
+              '"Noto Color Emoji"',
             ],
             mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-        },
-        extend: {
+          },
             colors: {
-                background: '#232946',
-                headline: '#fffffe',
-                paragraph: '#b8c1ec',
-                'paragraph-white': '#fefefe',
-                button: '#eebbc3',
-                'button-text': '#232946',
-                stroke: '#121629',
-                main: '#b8c1ec',
-                highlight: '#eebbc3',
-                secondary: '#fffffe',
-                tertiary: '#eebbc3',
-            'light-background': '#d4d8f0',
-                'light-accent': '#f0e2e1',
-
-                'rich-black': '#0C1824',
-                'blue-green': '#77CBB9',
-                orange: '#FFA552'
+              'rich-black': '#0C1824',
+              'eerie-black': '#222222',
+              'jet-black': '#333333',
+              'link-blue': '#457B9D',
+              'blue-green': '#77CBB9',
+              orange: '#FFA552'
 
             },
         },
